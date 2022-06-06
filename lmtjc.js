1bcc87d9517fb2d21fc49bc31307405937cbda22
xiaoheiwa1031 <99459877+xiaoheiwa1031@users.noreply.github.com>
2022-06-06 19:01:56 +0800

    Update lmtjc.js

--- lmtjc.js
+++ lmtjc.js
@@ -37,8 +37,8 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
     let panel_result = {
       title: '流媒体检测',
       content: '',
-      icon: 'hand.thumbsup',
-      'icon-color': '#0047AB',
+      icon: 'play.tv.fill',
+      'icon-color': '#FF2D55',
     }
   let [{ region, status }] = await Promise.all([testDisneyPlus()])
     await Promise.all([check_youtube_premium(),check_netflix()])

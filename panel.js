38e6a0ade4e1eca97e03d2d15124564c549ff62d
xiaoheiwa1031 <99459877+xiaoheiwa1031@users.noreply.github.com>
2022-06-09 18:46:08 +0800

    Update panel.js

--- panel.js
+++ panel.js
@@ -15,7 +15,7 @@ if ($trigger == "button") {
 	$notification.post("配置重载","配置重载成功","")
 };
 $done({
-    title:"“趴趴丶熊灬”的 𝓢𝓾𝓻𝓰𝓮 ᴾᴿᴼ  已运行"+startTime,
+    title:"“趴趴丶熊灬”的 𝓢𝓾𝓻𝓰𝓮 ᴾᴿᴼ      已运行"+startTime,
     content:"Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
     icon: icon_s?"crown.fill":"info.circle.fill",
    "icon-color":icon_s?"#f6c970":"#FF7500"

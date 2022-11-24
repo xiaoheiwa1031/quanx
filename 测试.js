e8309debc91e31457fc127fde718a535ffbf6fee
xiaoheiwa1031 <99459877+xiaoheiwa1031@users.noreply.github.com>
2022-11-24 13:45:56 +0800

    Update 测试.js

--- 测试.js
+++ 测试.js
@@ -1,4 +1,12 @@
-#^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/(\$RCAnonymousID%)?(\w)*$) url script-echo-response https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/revenuecat.js
-https?:\/\/revenuecat\.lakecoloring\.com\/v\d\/(receipts|subscribers) url script-echo-response https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/lakepro.js
-;https?:\/\/api\.lightricks\.com\/subscription\?bundle=com\.lightricks\.Enlight-Video url script-echo-response https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/videoleap.js
-https?:\/\/screen-lock\.51wnl-cq\.com\/userApi\/saveUser url script-echo-response https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/budingsuoping.
+[Rewrite]
+
+# ～ Not Boring 四件套[天气、习惯、日历、时间] 解锁会员权限（2022-11-23）@ddgksf2013
+^https?:\/\/api-weather\.andy\.works\/v\d\/\w{13,18}$ reject-dict
+
+[Script]
+
+http-request ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/*) script-path=https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/notboring.js, requires-body=false, timeout=10, tag=notboring
+
+[MITM]
+
+hostname = api-weather.andy.works,api.revenuecat.com

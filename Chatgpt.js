b92da1b0392c90bab80e719c31e7fdc4deeaff1a
xiaoheiwa1031 <99459877+xiaoheiwa1031@users.noreply.github.com>
2023-04-10 21:45:05 +0800

    Update Chatgpt.js

--- Chatgpt.js
+++ Chatgpt.js
@@ -1,3 +1,4 @@
+
 let url = "http://chat.openai.com/cdn-cgi/trace"
 tf=["T1","XX","AL","DZ","AD","AO","AG","AR","AM","AU","AT","AZ","BS","BD","BB","BE","BZ","BJ","BT","BA","BW","BR","BG","BF","CV","CA","CL","CO","KM","CR","HR","CY","DK","DJ","DM","DO","EC","SV","EE","FJ","FI","FR","GA","GM","GE","DE","GH","GR","GD","GT","GN","GW","GY","HT","HN","HU","IS","IN","ID","IQ","IE","IL","IT","JM","JP","JO","KZ","KE","KI","KW","KG","LV","LB","LS","LR","LI","LT","LU","MG","MW","MY","MV","ML","MT","MH","MR","MU","MX","MC","MN","ME","MA","MZ","MM","NA","NR","NP","NL","NZ","NI","NE","NG","MK","NO","OM","PK","PW","PA","PG","PE","PH","PL","PT","QA","RO","RW","KN","LC","VC","WS","SM","ST","SN","RS","SC","SL","SG","SK","SI","SB","ZA","ES","LK","SR","SE","CH","TH","TG","TO","TT","TN","TR","TV","UG","AE","US","UY","VU","ZM","BO","BN","CG","CZ","VA","FM","MD","PS","KR","TW","TZ","TL","GB"]
 tff=["plus","on"]
@@ -31,20 +32,20 @@ let loc = cf.loc
 //loc
 let l = tf.indexOf(loc)
 if (l != -1) {
-	gpt = "GPT: ✅已支持"
+	gpt = "GPT状态:                 已支持✅"
 } else {
-	gpt = "GPT: ❎未支持"
+	gpt = "GPT状态:                 未支持❎"
 }
 //warp
 let w = tff.indexOf(warp)
 if (w != -1) {
-	warps = "✅已开启"
+	warps = "                 已开启✅"
 } else {
-	warps = "❎未开启"
+	warps = "                 未开启 ❎"
 }
 body = {
 title: "Chatgpt检测",
-content: `${gpt}\n区域: ${loc}\nWarp: ${warps}`,
+content: `${gpt}\nWarp状态: ${warps}\n节点区域:                     ${loc}`,
   icon: iconUrl,
   'icon-color': iconColor,
 },$done(body);})
